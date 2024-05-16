71e46bc29a19e3a100f39372de9d4ed018e8606e
JEAN ANCON <terry.com>
2024-05-16 17:07:13 -0500

    nueva funcion

--- p7-bucles.php
+++ p7-bucles.php
@@ -1,22 +1,29 @@
 <?php
 
-$contador=1;
-$sumanotas=0;
-while($contador<=3){
-    echo "ingrese sus notas : ";
-    $nota = fgets(STDIN);
-    $sumanotas = $sumanotas + $nota;
+$contador = 1;
+$totalNotes = 0;
+$menor1 = PHP_INT_MAX; 
+$mayor1 = PHP_INT_MIN; 
+
+while ($contador <= 3) {
+    echo "Ingrese sus notas: ";
+    $note = fgets(STDIN);
+    $totalNotes += $note;
     $contador++;
 }
-$notapromedio = $sumanotas/3;
-echo "\n El promedio de notas es : ".$notapromedio."\n";
-for ($i = 1; $i <= 5; $i++){
-    echo "ingrese un numero : ";
-    $numero = fgets(STDIN);
-    if ($mayor < $numero){
-        $mayor = $numero;
+$notapromedio = $totalNotes / 3;
+echo "\nEl promedio de notas es: $notapromedio\n";
+
+for ($i = 1; $i <= 5; $i++) {
+    echo "Ingrese un número: ";
+    $number = fgets(STDIN);
+    if ($number > $mayor1) {
+        $mayor1 = $number;
+    }
+    if ($number < $menor1) {
+        $menor1 = $number;
     }
 }
-echo "el numero mayor es : ".$mayor;
-
+echo "El número mayor es: $mayor1\n";
+echo "El número menor es: $menor1\n";
 ?>
