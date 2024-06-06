f4684a7bc3310de7309cc3e248c5fb98b30491ed
juliodavl10 <1546589@senati.pe>
2024-06-06 15:06:41 -0500

    nueva funcion

---
+++ p22-lista-pdo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Busqueda de Pacientes</title>
+</head>
+<body>
+    <center><h1>Busqueda de Pacientes</h1></center>
+    <form action="p22.1-consulta-pdo.php" method="GET">
+        <input type="text" name="search_query" placeholder="Escribe aquí">
+        <button type="submit">Buscar</button>
+    </form>
+    
+</body>
+</html>
